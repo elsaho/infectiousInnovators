72360c926ed8d0a4ebfee6c3a6071c3124ad06ef
elsaho <elsa.ho98@gmail.com>
2023-01-22 02:05:31 -0800

    implemented logout functionality

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,7 +1,7 @@
 function logout() {
     console.log("logging out user");
     firebase.auth().signOut().then(() => {
-        window.location.href = "login.html";
+        window.location.href = "../index.html";
       }).catch((error) => {
       });
   }
