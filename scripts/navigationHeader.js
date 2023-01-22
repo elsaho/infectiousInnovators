878eb844239d23305b2b88e5dbcef45f30d75a99
elsaho <elsa.ho98@gmail.com>
2023-01-22 01:46:09 -0800

    fixed settings page bug

--- scripts/navigationHeader.js
+++ scripts/navigationHeader.js
@@ -1,5 +1,10 @@
 function topNavigation(){
     const home = document.getElementById("profile");
+
+    function goMain(){
+      window.location.href = "/html/main.html";
+    }
+    home.addEventListener("click", goMain);
   
     function goProfile(){
       window.location.href = "/html/settings.html";
