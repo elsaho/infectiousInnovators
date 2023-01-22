a2d3b04ca101576c56ef82c884a6b7976118b4eb
Tomasz Stojek <stojek604@gmail.com>
2023-01-22 06:06:39 -0800

    added a listen function

--- scripts/chats.js
+++ scripts/chats.js
@@ -1,15 +1,18 @@
 
 // Used to make divs clickable, current changes inner html to test and print
+listenPlease();
+function listenPlease() {
 var clickedChat = document.querySelector('.clickableClass');
 
 clickedChat.addEventListener('click', function(event) {
-    document.getElementById("clickableID").innerHTML = "YOU CLICKED ME!"
+    document.getElementById("clickableID").innerHTML = "YOU CLICKED ONe OF THE MATCHES TOP ONE WILL CHANGE !"
     console.log("the console log message", this.id);
     /// function that populates the chat dynamically which is passed the this,id which is will be the matchID / chatID
     var selectedChat = this.id;
 
    
 });
+}
 
 function populateChat(selectedChat) {
 
@@ -64,6 +67,7 @@ function makeCardMatchesFromTemplate(arg) {
     .catch((error) => {
         console.log("Error getting documents: ", error);
     });
+    
     }
  }
  
