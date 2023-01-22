735509ad3ceb72f293a49b8a9b0dec5a11c0b953
Tomasz Stojek <stojek604@gmail.com>
2023-01-22 05:51:06 -0800

    changed match card format

--- scripts/chats.js
+++ scripts/chats.js
@@ -45,14 +45,20 @@ firebase.auth().onAuthStateChanged(user => {
 }
 
 function makeCardMatchesFromTemplate(arg) {
+    var parkingspotCardTemplate = document.getElementById("sideBarMatch");
+    var parkingspotCardGroup = document.getElementById("matchGroup");
     for (i = 0; i < arg.length; i++) {
         console.log("im inside the loop lol", arg[i]);
         db.collection("users").where("userID", "==", arg[i])
     .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
+            var name = doc.data().name;
+            console.log("tyring to print the name", name);
             console.log(doc.id, " => ", doc.data());
+            let matchCard = parkingspotCardTemplate.content.cloneNode(true);
+            matchCard.querySelector(".nameClass").innerHTML = name;
+            parkingspotCardGroup.appendChild(matchCard);
         });
     })
     .catch((error) => {
