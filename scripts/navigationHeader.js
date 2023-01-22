ddbb9364b6bc0181a400f141ac73056983c98f2e
elsaho <elsa.ho98@gmail.com>
2023-01-22 01:58:27 -0800

    updated colour of card on main

--- scripts/navigationHeader.js
+++ scripts/navigationHeader.js
@@ -1,10 +1,11 @@
 function topNavigation(){
     const home = document.getElementById("profile");
+    const main = document.getElementById("title");
 
     function goMain(){
       window.location.href = "/html/main.html";
     }
-    home.addEventListener("click", goMain);
+    main.addEventListener("click", goMain);
   
     function goProfile(){
       window.location.href = "/html/settings.html";
