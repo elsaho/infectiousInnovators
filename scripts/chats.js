fca88e864fafb8c0eb5c49b7606e69af09a8261c
Tomasz Stojek <stojek604@gmail.com>
2023-01-22 06:37:36 -0800

    changed two lines for QueryALL

--- scripts/chats.js
+++ scripts/chats.js
@@ -1,17 +1,17 @@
-
 // Used to make divs clickable, current changes inner html to test and print
-listenPlease();
+
+
 function listenPlease() {
-var clickedChat = document.querySelector('.clickableClass');
+    var clickedChat = document.querySelectorAll('.clickableClass');
+    console.log("tyring to print the nodelist", clickedChat);
+    clickedChat.forEach((matchBox) => {
+        matchBox.addEventListener('click', function (event) {
+            document.getElementById("clickableID").innerHTML = "YOU CLICKED ONe OF THE MATCHES TOP ONE WILL CHANGE !"
 
-clickedChat.addEventListener('click', function(event) {
-    document.getElementById("clickableID").innerHTML = "YOU CLICKED ONe OF THE MATCHES TOP ONE WILL CHANGE !"
-    console.log("the console log message", this.id);
-    /// function that populates the chat dynamically which is passed the this,id which is will be the matchID / chatID
-    var selectedChat = this.id;
 
-   
-});
+
+        });
+    });
 }
 
 function populateChat(selectedChat) {
@@ -23,20 +23,20 @@ var currentUser;
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      currentUser = db.collection("users").doc(user.uid); //global
-      console.log(currentUser);
-      populateMatches();
-    
+        currentUser = db.collection("users").doc(user.uid); //global
+        console.log(currentUser);
+        populateMatches();
+
     } else {
-      // No user is signed in.
-      console.log("No user is signed in");
-      window.location.href = "login.html";
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
     }
-  });
+});
 
- 
 
-  function populateMatches(id) {
+
+function populateMatches(id) {
     currentUser.get().then((userDoc) => {
         matchList = userDoc.data().matches;
         console.log(matchList);
@@ -48,30 +48,26 @@ firebase.auth().onAuthStateChanged(user => {
 }
 
 function makeCardMatchesFromTemplate(arg) {
+    
     var parkingspotCardTemplate = document.getElementById("sideBarMatch");
     var parkingspotCardGroup = document.getElementById("matchGroup");
     for (i = 0; i < arg.length; i++) {
         console.log("im inside the loop lol", arg[i]);
         db.collection("users").where("userID", "==", arg[i])
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            var name = doc.data().name;
-            console.log("tyring to print the name", name);
-            console.log(doc.id, " => ", doc.data());
-            let matchCard = parkingspotCardTemplate.content.cloneNode(true);
-            matchCard.querySelector(".nameClass").innerHTML = name;
-            parkingspotCardGroup.appendChild(matchCard);
-        });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
-    
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    var name = doc.data().name;
+                    console.log("tyring to print the name", name);
+                    console.log(doc.id, " => ", doc.data());
+                    let matchCard = parkingspotCardTemplate.content.cloneNode(true);
+                    matchCard.querySelector(".nameClass").innerHTML = name;
+                    parkingspotCardGroup.appendChild(matchCard);
+                });
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
+
     }
- }
- 
-
-
-
-
+}
